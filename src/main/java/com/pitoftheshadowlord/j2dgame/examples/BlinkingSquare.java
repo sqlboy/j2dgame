4d830a71c98b64ba9814f60cfaebaa04d588246b
Matt Chambers <mvchambers@me.com>
2013-11-24 00:00:47 +1300

    removed unessesary commented code.

--- src/main/java/com/pitoftheshadowlord/j2dgame/examples/BlinkingSquare.java
+++ src/main/java/com/pitoftheshadowlord/j2dgame/examples/BlinkingSquare.java
@@ -7,36 +7,22 @@ import com.pitoftheshadowlord.j2dgame.core.JGObject;
 
 public class BlinkingSquare extends JGObject {
 
-	private Color color = new Color(0, 0, 0);
+    private Color color = new Color(0, 0, 0);
 
-	public BlinkingSquare(int x, int y, int size) {
-		super(x, y, size, size);
-		rateLimit.setRate(5);
-	}
-	
-	public void render(Graphics g) {
-		g.setColor(color);
-		g.fillRect(rect.x, rect.y, rect.width, rect.height);
-	}
-	
-	public void update() {
-		//condition ? value_if_true : value_if_false
-				
-		if (rateLimit.tryAcquire()) {
-			color = color.equals(Color.red) ? Color.pink : Color.red;
-		}
+    public BlinkingSquare(int x, int y, int size) {
+        super(x, y, size, size);
+        rateLimit.setRate(5);
+    }
 
+    public void render(Graphics g) {
+        g.setColor(color);
+        g.fillRect(rect.x, rect.y, rect.width, rect.height);
+    }
 
-		/*
-		if (color.getRed() >= 255) {
-			rate =  rate * -1;
-		} else if (color.getRed() <= 0) {
-			rate = rate * -1;
-		}
-		
-		int c = Math.max(0, Math.min(255, color.getRed() + rate));		
-		color = new Color(c, c, c);
-		*/
-	}
+    public void update() {
+        if (rateLimit.tryAcquire()) {
+            color = color.equals(Color.red) ? Color.pink : Color.red;
+        }
+    }
 
 }
