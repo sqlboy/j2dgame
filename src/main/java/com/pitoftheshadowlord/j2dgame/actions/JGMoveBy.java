16df20ad324a725423bfa8e78af430b748609ab0
Matt Chambers <mvchambers@me.com>
2013-11-24 22:18:06 +1300

    Fixes issue where roudning error caused movements to be 1 or 2 pixels off.

---
+++ src/main/java/com/pitoftheshadowlord/j2dgame/actions/JGMoveBy.java
@@ -0,0 +1,79 @@
+package com.pitoftheshadowlord.j2dgame.actions;
+
+import java.awt.Point;
+
+import com.pitoftheshadowlord.j2dgame.core.JGObject;
+
+public class JGMoveBy extends JGDurationAction {
+
+    private double totalTime = 0;
+
+    protected int movex;
+    protected int movey;
+
+    protected float xmovePerTick;
+    protected float ymovePerTick;
+
+    private float xmoveBuffer = 0f;
+    private float ymoveBuffer = 0f;
+
+    private int xmovePixels = 0;
+    private int ymovePixels = 0;
+
+    public JGMoveBy(float duration, Point vec) {
+        super(duration);
+        this.movex = vec.x;
+        this.movey = vec.y;
+    }
+
+    public JGMoveBy(float duration, int x, int y) {
+        super(duration);
+        this.movex = x;
+        this.movey = y;
+    }
+
+    public void begin() {}
+    public void end() {}
+
+    @Override
+    public void update(double delta) {
+        if (done) {
+            return;
+        }
+
+        if (totalTime < durationTime) {
+            if (totalTime + delta > durationTime) {
+                delta = (durationTime - totalTime) + 0.1;
+            }
+
+            totalTime+=(delta);
+
+            xmoveBuffer+=xmovePerTick * delta;
+            ymoveBuffer+=ymovePerTick * delta;
+
+            if (xmoveBuffer >= 1 || xmoveBuffer <=-1) {
+                xmovePixels = (int) xmoveBuffer;
+                xmoveBuffer = xmoveBuffer - xmovePixels;
+                target.moveX(xmovePixels);
+            }
+
+            if (ymoveBuffer >= 1 || ymoveBuffer <=-1) {
+                ymovePixels = (int) ymoveBuffer;
+                ymoveBuffer = ymoveBuffer - ymovePixels;
+                target.moveY(ymovePixels);
+            }
+        }
+        else {
+            done = true;
+        }
+    }
+
+    public void setTarget(JGObject target) {
+        this.target = target;
+
+        // calculate the pixel movement per second.
+        xmovePerTick = movex / durationTime;
+        ymovePerTick = movey / durationTime;
+    }
+
+}
--- src/main/java/com/pitoftheshadowlord/j2dgame/actions/JGMoveTo.java
+++ src/main/java/com/pitoftheshadowlord/j2dgame/actions/JGMoveTo.java
@@ -4,65 +4,23 @@ import java.awt.Point;
 
 import com.pitoftheshadowlord.j2dgame.core.JGObject;
 
-public class JGMoveTo extends JGDurationAction {
+public class JGMoveTo extends JGMoveBy {
 
-    private Point dstPoint;
     private Point srcPoint;
 
-    private double totalTime = 0;
-
-    private float xmovePerTick;
-    private float ymovePerTick;
-
-    private float xmoveBuffer = 0f;
-    private float ymoveBuffer = 0f;
-
-    private int xmovePixels = 0;
-    private int ymovePixels = 0;
-
     public JGMoveTo(float duration, Point point) {
-        super(duration);
-        this.dstPoint = point;
+        super(duration, point);
     }
 
     public void begin() {}
     public void end() {}
-
-    @Override
-    public void update(double delta) {
-        if (done) {
-            return;
-        }
-
-        if (totalTime < durationTime) {
-            totalTime+=(delta);
-
-            xmoveBuffer+=xmovePerTick * delta;
-            ymoveBuffer+=ymovePerTick * delta;
-
-            if (xmoveBuffer >= 1 || xmoveBuffer <=-1) {
-                xmovePixels = (int) xmoveBuffer;
-                xmoveBuffer = xmoveBuffer - xmovePixels;
-                target.moveX(xmovePixels);
-            }
-
-            if (ymoveBuffer >= 1 || ymoveBuffer <=-1) {
-                ymovePixels = (int) ymoveBuffer;
-                ymoveBuffer = ymoveBuffer - ymovePixels;
-                target.moveY(ymovePixels);
-            }
-        }
-        else {
-            done = true;
-        }
-    }
 
     public void setTarget(JGObject target) {
         this.target = target;
         this.srcPoint = new Point(target.getPosition());
 
         // calculate the pixel movement per second.
-        xmovePerTick = (dstPoint.x - srcPoint.x) / durationTime;
-        ymovePerTick = (dstPoint.y - srcPoint.y) / durationTime;
+        xmovePerTick = (movex - srcPoint.x) / durationTime;
+        ymovePerTick = (movey - srcPoint.y) / durationTime;
     }
 }
