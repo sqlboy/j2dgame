95247eb87bfad994c470c70cfdcdf09b8e779cc3
Matt Chambers <mvchambers@me.com>
2013-11-27 22:02:51 +1300

    Added ranged dice roll.

--- src/main/java/com/pitoftheshadowlord/j2dgame/core/Dice.java
+++ src/main/java/com/pitoftheshadowlord/j2dgame/core/Dice.java
@@ -9,4 +9,11 @@ public class Dice {
     public static final int roll(int max) {
         return RANDOM.nextInt(max) + 1;
     }
+
+    public static final int roll(int min, int max) {
+        if (max - min + 1 <= 0) {
+            return 1;
+        }
+        return RANDOM.nextInt(max - min + 1) + min;
+    }
 }
