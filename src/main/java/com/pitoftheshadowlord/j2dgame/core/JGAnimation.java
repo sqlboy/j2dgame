0495bc2d2507a7a13c8c7acf783841c4ce08a071
Matt Chambers <mvchambers@me.com>
2013-12-02 07:25:27 +1300

    Added ability to set individual frames with an animation.

--- src/main/java/com/pitoftheshadowlord/j2dgame/core/JGAnimation.java
+++ src/main/java/com/pitoftheshadowlord/j2dgame/core/JGAnimation.java
@@ -9,23 +9,31 @@ import com.pitoftheshadowlord.j2dgame.sprites.SpriteSheet;
 public class JGAnimation extends JGObject {
 
     private List<BufferedImage> frames;
-    private boolean oneShot;
-    private boolean done;
+    private boolean oneShot = false;
+    private boolean done = false;
     private int frame = 0;
     private int frameCount;
+    private float duration = 0;
 
     public JGAnimation(List<BufferedImage> frames, float duration, int x, int y) {
-        super(frames.get(0).getWidth(), frames.get(0).getHeight(), x, y);
+        super(x, y, frames.get(0).getWidth(), frames.get(0).getHeight());
+        this.setPosition(x, y);
         this.frames = frames;
         this.frameCount = frames.size();
-        this.rateLimit.setRate(frames.size() / duration);
+        this.duration = duration;
+        if (duration > 0) {
+            this.rateLimit.setRate(frames.size() / duration);
+        }
     }
 
     public JGAnimation(SpriteSheet sheet, float duration, int x, int y) {
-        super(sheet.getFrames().get(0).getWidth(), sheet.getFrames().get(0).getHeight(), x, y);
+        super(x, y, sheet.getFrames().get(0).getWidth(), sheet.getFrames().get(0).getHeight());
         this.frames = sheet.getFrames();
         this.frameCount = frames.size();
-        this.rateLimit.setRate(frames.size() / duration);
+        this.duration = duration;
+        if (duration > 0) {
+            this.rateLimit.setRate(frames.size() / duration);
+        }
     }
 
     public JGAnimation oneShot() {
@@ -42,7 +50,7 @@ public class JGAnimation extends JGObject {
     }
 
     public void update() {
-        if (done || !rateLimit.tryAcquire()) {
+        if (duration == 0 || done || !rateLimit.tryAcquire()) {
             return;
         }
 
@@ -59,4 +67,10 @@ public class JGAnimation extends JGObject {
     public void render(Graphics g) {
          g.drawImage(frames.get(frame), rect.x, rect.y, null);
     }
+
+    public JGAnimation setFrame(int frame) {
+        this.frame = frame;
+        return this;
+    }
+
 }
